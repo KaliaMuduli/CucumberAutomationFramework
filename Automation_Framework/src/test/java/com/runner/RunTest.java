536c864f645657b7082a143e0110e1ca2934ab4d
KaliaMuduli <kanha.jyo@gmail.com>
2022-08-07 09:47:52 +0530

    Tag Demo

--- Automation_Framework/src/test/java/com/runner/RunTest.java
+++ Automation_Framework/src/test/java/com/runner/RunTest.java
@@ -8,9 +8,12 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",glue= {"com.StepDefinations"},monochrome=true,
                  plugin= {"html:Reports\\HtmlReport","json:Reports\\JSONReport\\cucumber.json","junit:Reports\\JUNITReport\\cucumber.xml"},
-                 dryRun=false
-                 
-		
+                 dryRun=false, //Mapping between feature file and steps file
+                // strict=true   //It will check if any step is not defined in step def file
+                 tags="@SmokeTest or @SanityTest"
+		     //  tags="@SmokeTest and @SanityTest"
+              //  tags="@SmokeTest and not @SanityTest"
+		     //  tags="not @SanityTest"
 		)
 
 public class RunTest {
---
+++ Automation_Framework/src/test/resources/Features/TagDemo.feature
@@ -0,0 +1,22 @@
+Feature: Test the login using tag Demo
+
+  @SmokeTest
+  Scenario: Test1
+    Given User is on Login Page
+
+  @SanityTest
+  Scenario: Test2
+    Given User is on Login Page
+
+  Scenario: Test3
+    Given User is on Login Page
+
+  @SmokeTest @SanityTest
+  Scenario: Test4
+    Given User is on Login Page
+
+  Scenario: Test5
+    Given User is on Login Page
+
+  Scenario: Test6
+    Given User is on Login Page
